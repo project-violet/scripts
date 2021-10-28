d20b1541d0d198c6a3b9d19795ca78f1747a3881
Violet <66541802+violet-dev@users.noreply.github.com>
2021-10-28 19:25:32 +0900

    Update hitomi_get_image_list.js

--- hitomi_get_image_list.js
+++ hitomi_get_image_list.js
@@ -6,7 +6,7 @@
 // Output: $result [Map] - Image List, Thumbnail List
 
 function create_download_url(id) {
-  return "https://ltn.hitomi.la/galleries" + id + ".js";
+  return "https://ltn.hitomi.la/galleries/" + id + ".js";
 }
 
 function hitomi_get_image_list(id, gg) {
