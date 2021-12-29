1f8ef094f828ff1c91930bf36a1c24e0acdc26f7
Violet <66541802+violet-dev@users.noreply.github.com>
2021-12-29 19:54:01 +0900

    Update hitomi_get_image_list.js

--- hitomi_get_image_list.js
+++ hitomi_get_image_list.js
@@ -335,7 +335,7 @@ function hitomi_get_image_list(id, gg) {
     postfix = hash.substr(hash.length - 3);
     subdomainx = subdomain;
     if (rr["haswebp"] == 0 || rr["haswebp"] == null) subdomainx = "b";
-    x = parseInt(postfix[0] + postfix[1], 16);
+    x = parseInt(postfix[2] + postfix[0] + postfix[1], 16);
     if (!isNaN(x)) {
       if (x < 4096) subdomainx = String.fromCharCode(97 + gg_m[x]);
       else subdomainx = String.fromCharCode(97 + 0);
