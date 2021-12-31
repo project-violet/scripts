19d73e6e579c90e9e00aba85cd79aafe30338f6e
violet-dev <violet.dev.master@gmail.com>
2021-12-31 07:48:45 +0000

    ?

---
+++ sync.py
@@ -0,0 +1,15 @@
+# This source code is a part of Project Violet.
+# Copyright (C) 2021. violet-team. Licensed under the Apache-2.0 License.
+
+def sync():
+    f = open('hitomi_get_image_list_model.js', 'r')
+    gg = open('gg.js', 'r')
+
+    r = f.read().replace("%%1%", gg.read())
+
+    f.close()
+    f = open('hitomi_get_image_list_v2.js', 'w+')
+    f.write(r)
+    f.close()
+
+sync()
