0e2ff6d67bbe110e0317dc0707db4b70013e0207
Violet <66541802+violet-dev@users.noreply.github.com>
2022-08-09 10:22:41 +0900

    Create hitomi_get_image_list_v4_model.js

---
+++ hitomi_get_image_list_v4_model.js
@@ -0,0 +1,93 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2022. violet-team. Licensed under the Apache-2.0 License.
+//
+// Script Name: hitomi_get_image_list.js
+// Input:  $id     [int] - Hitomi.la Article Id
+// Output: $result [Map] - Image List, Thumbnail List
+
+function create_download_url(id) {
+  return "https://ltn.hitomi.la/galleries/" + id + ".js";
+}
+
+function gg_s(h) { var m = /(..)(.)$/.exec(h); return parseInt(m[2]+m[1], 16).toString(10); }
+
+var gg_m = [%%gg.m%];
+var gg_b = "%%gg.b%";
+
+function hitomi_get_image_list() {
+  files = galleryinfo["files"];
+  // function test(id, files) {
+  number_of_frontends = 3;
+  subdomain = "a";
+  btresult = [];
+  stresult = [];
+  result = [];
+  for (var rr of files) {
+    hash = rr["hash"];
+    postfix = hash.substr(hash.length - 3);
+    subdomainx = subdomain;
+    if (rr["haswebp"] == 0 || rr["haswebp"] == null) subdomainx = "b";
+    x = parseInt(postfix[2] + postfix[0] + postfix[1], 16);
+    if (!isNaN(x)) {
+      if (x < 4096) subdomainx = String.fromCharCode(97 + gg_m[x]);
+      else subdomainx = String.fromCharCode(97 + 0);
+    }
+    if (rr["haswebp"] == 0 || rr["haswebp"] == null) {
+      result.push(
+        `https://${subdomainx}b.hitomi.la/images/${gg_b + gg_s(hash)}/${hash}.${
+          rr["name"].split(".").slice(-1)[0]
+        }`
+      );
+    } else if (hash == "")
+      result.push(`https://${subdomainx}a.hitomi.la/webp/${rr["name"]}.webp`);
+    else if (hash.length < 3)
+      result.push(`https://${subdomainx}a.hitomi.la/webp/${hash}.webp`);
+    else {
+      result.push(
+        `https://${subdomainx}a.hitomi.la/webp/${
+            gg_b + gg_s(hash)
+        }/${hash}.webp`
+      );
+    }
+    if (rr["haswebp"] == 1)
+    {
+      btresult.push(
+        `https://tn.hitomi.la/webpbigtn/${postfix[2]}/${postfix[0]}${postfix[1]}/${hash}.webp`
+      );
+      stresult.push(
+        `https://${subdomainx}tn.hitomi.la/webpsmalltn/${postfix[2]}/${postfix[0]}${postfix[1]}/${hash}.webp`
+      );
+    }
+    // else if (rr["hasavif"] == 1)
+    // {
+    //   btresult.push(
+    //     `https://tn.hitomi.la/avifbigtn/${postfix[2]}/${postfix[0]}${postfix[1]}/${hash}.avif`
+    //   );
+    //   stresult.push(
+    //     `https://${subdomainx}tn.hitomi.la/avifsmallbigtn/${postfix[2]}/${postfix[0]}${postfix[1]}/${hash}.avif`
+    //   );
+    // }
+    else 
+    {
+      btresult.push(
+        `https://tn.hitomi.la/bigtn/${postfix[2]}/${postfix[0]}${postfix[1]}/${hash}.jpg`
+      );
+      stresult.push(
+        `https://${subdomainx}tn.hitomi.la/smalltn/${postfix[2]}/${postfix[0]}${postfix[1]}/${hash}.jpg`
+      );
+    }
+  }
+  return JSON.stringify({
+    btresult: btresult,
+    stresult: stresult,
+    result: result,
+  });
+}
+
+function hitomi_get_header_content(id) {
+  return JSON.stringify({
+      'referer': `https://hitomi.la/reader/${id}.html`,
+      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.3',
+  });
+}
