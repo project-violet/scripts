a6d5b89cecb58aad9a83d747413326d9b7d47984
violet-dev <violet.dev.master@gmail.com>
2023-02-12 06:04:07 +0000

    update

--- hitomi_get_image_list_v3.js
+++ hitomi_get_image_list_v3.js
@@ -2366,7 +2366,7 @@ function hitomi_get_image_list() {
     postfix = hash.substr(hash.length - 3);
     subdomainx = subdomain;
     if (rr["haswebp"] == 0 || rr["haswebp"] == null) subdomainx = "b";
-    x = parseInt(postfix[2] + postfix[1] + postfix[0], 16);
+    x = parseInt(postfix[2] + postfix[0] + postfix[1], 16);
     if (!isNaN(x)) {
       subdomainx = String.fromCharCode(97 + gg_m[x]);
     }
